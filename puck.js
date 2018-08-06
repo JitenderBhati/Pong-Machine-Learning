363cf2206c3d88f2704ab5ec9ce6850b4b2f6f56
unknown <jkrajput24@gmail.com>
2018-08-06 06:05:07 +0530

    Adding AI and customization

--- puck.js
+++ puck.js
@@ -18,8 +18,8 @@ class Puck {
                 let diff = this.y - (p.y - p.h/2);
                 let rad = radians(45);
                 let angle = map(diff, 0, p.h, -rad, rad);
-                this.xspeed = 5 * cos(angle);
-                this.yspeed = 5 * sin(angle);
+                this.xspeed = 10 * cos(angle);
+                this.yspeed = 10 * sin(angle);
                 this.x = p.x + p.w/2 + this.r;
             }
         }
@@ -32,13 +32,24 @@ class Puck {
             if (this.x < p.x) {
                 let diff = this.y - (p.y - p.h/2);
                 let angle = map(diff, 0, p.h, radians(225), radians(135));
-                this.xspeed = 5 * cos(angle);
-                this.yspeed = 5 * sin(angle);
+                this.xspeed = 10 * cos(angle);
+                this.yspeed = 10 * sin(angle);
                 this.x = p.x - p.w/2 - this.r;
             }
         }
     }
-
+    computerMovement(p)
+    {
+      var paddle2YCenter= p.y + p.h/2;
+      if(paddle2YCenter < this.y-35)
+      {
+        p.move(10);
+      }
+      else if(paddle2YCenter > this.y+35)
+      {
+        p.move(-10);
+      }
+    }
     update() {
         this.x += this.xspeed;
         this.y += this.yspeed;
@@ -48,8 +59,8 @@ class Puck {
         this.x = width/2;
         this.y = height/2;
         let angle = random(-PI/4, PI/4);
-        this.xspeed = 5 * Math.cos(angle);
-        this.yspeed = 5 * Math.sin(angle);
+        this.xspeed = 10 * Math.cos(angle);
+        this.yspeed = 10 * Math.sin(angle);
 
         if (random(1) < 0.5) {
             this.xspeed *= -1;
@@ -62,13 +73,13 @@ class Puck {
         }
 
         if (this.x - this.r > width) {
-            // ding.play();
+            ding.play();
             leftscore++;
             this.reset();
         }
 
         if (this.x + this.r < 0) {
-            //ding.play();
+            ding.play();
             rightscore++;
             this.reset();
         }
--- sketch.js
+++ sketch.js
@@ -1,29 +1,33 @@
 let video, classifier, label='';
-let lUButton, lDButton;
-let featureExtractor, train;
+// let lUButton, lDButton, train;
+let featureExtractor;
 let leftscore = 0;
 let rightscore = 0;
-let count=0, count1=0, count2=0, count3=0;
+let count=0, count1=0;
 var flag = false;
+// let lUButton = document.getElementsByClassName("button");
+// let lDButton = document.getElementsByClassName("button2");
+// const train = document.getElementById("train");
+
 
 function setup() {
 
     video = createCapture(VIDEO);
     createCanvas(600, 400);
-    // ding = loadSound("data/ding.mp3");
+    ding = loadSound("./data/ding.mp3");
     puck = new Puck();
     left = new Paddle(true);
     right = new Paddle(false);
 
     video.size(550, 400);
 
-    lUButton = createButton('Left Up');
-    lUButton.position(10, 450);
-    lDButton = createButton('Left Down');
-    lDButton.position(10, 480);
-
-    train = createButton('Train');
-    train.position(50, 530);
+    // lUButton = createButton('Left Up');
+    // lUButton.position(10, 450);
+    // lDButton = createButton('Left Down');
+    // lDButton.position(10, 480);
+    //
+    // train = createButton('Train');
+    // train.position(50, 530);
 
     featureExtractor = ml5.featureExtractor('MobileNet', ()=>{
       console.log("Model is ready");
@@ -33,30 +37,62 @@ function setup() {
       console.log("Video is Ready");
     });
 
-    lUButton.mousePressed(()=>{
-     console.log("Left Up: ",count);
-      count++;
-      classifier.addImage('Left Up');
-    });
+//     lUButton.mousePressed(()=>{
+//      console.log("Left Up: ",count);
+//       count++;
+//       classifier.addImage('Left Up');
+//     });
+//
+//   lDButton.mousePressed(()=>{
+//     console.log("Left Down: ", count1);
+//     count1++;
+//     classifier.addImage('Left Down');
+// });
 
-  lDButton.mousePressed(()=>{
+
+  // train.mousePressed(()=>{
+  //    classifier.train((loss)=>{
+  //     if(loss==null)
+  //     {
+  //       alert("Ready To Play");
+  //       classifier.classify(gotResult);
+  //       flag  = true;
+  //     }
+  //     console.log(loss);
+  //     })
+  //  });
+  }
+
+function leftUp()
+  {
+    console.log("Left Up: ",count);
+    count++;
+    let lblUp = document.getElementById("lblUp").innerHTML = count+ " Images";
+    // lblUp.innerHTML = "hello";
+    classifier.addImage('Left Up');
+  }
+  function leftDown()
+  {
     console.log("Left Down: ", count1);
     count1++;
+    let lblDown = document.getElementById("lblDown").innerHTML = count1+" Images";
     classifier.addImage('Left Down');
-});
-
-
-  train.mousePressed(()=>{
-     classifier.train((loss)=>{
-      if(loss==null)
-      {
-        classifier.classify(gotResult);
-        flag  = true;
-      }
-      console.log(loss);
-      })
-   });
   }
+
+function trains()
+{
+  let training =    document.getElementById("train").innerHTML = "Training...";
+  classifier.train((loss)=>{
+   if(loss==null)
+   {
+       let training =    document.getElementById("train").innerHTML = "Train It!";
+     alert("Ready To Play");
+     classifier.classify(gotResult);
+     flag  = true;
+   }
+   console.log(loss);
+   })
+}
 
 function gotResult(err, res){
   if(err)
@@ -67,7 +103,6 @@ function gotResult(err, res){
     classifier.classify(gotResult);
   }
 }
-var rand;
 
 function draw() {
 
@@ -76,8 +111,8 @@ function draw() {
   fill(255);
   text(label, 20, height-60);
   if(flag==true){
-  rand = random(1, 3);
-  console.log(Math.floor(rand));
+  // rand = random(1, 3);
+  // console.log(Math.floor(rand));
   puck.checkPaddleRight(right);
   puck.checkPaddleLeft(left);
 
@@ -96,21 +131,25 @@ function draw() {
     text(rightscore, width-64, 40);
     if(leftscore==5)
     {
-      console.log("Left Player Wins");
+      // console.log("Left Player Wins");
+      flag = false;
+      isSomething("Left");
       leftscore=0;
       rightscore=0;
     }
     if(rightscore==5){
-      console.log("Right Player Wins");
+      flag = false;
+      isSomething("Right")
       leftscore=0;
       rightscore=0;
     }
-    if (Math.floor(rand)==1) {
-        right.move(-10);
-    }
-    else if (Math.floor(rand)==2) {
-        right.move(10);
-    }
+    puck.computerMovement(right);
+    // if (Math.floor(rand)==1) {
+    //     right.move(-10);
+    // }
+    // else if (Math.floor(rand)==2) {
+    //     right.move(10);
+    // }
     if (label == "Left Up") {
         left.move(-10);
     }
@@ -119,6 +158,35 @@ function draw() {
     }
   }
 }
+
+function isSomething(a)
+{
+  fill(255);
+  console.log("hehe");
+  textSize(20);
+  text(a + " Player Wins!", width/2-90, 50);
+  textSize(10);
+  text("Press 'Y' to Continue!", width/2-50, 100);
+  noLoop();
+}
+
+function keyPressed()
+{
+  if(key=='Y')
+  {
+    flag = true;
+    loop();
+  }
+}
+
+// function computerMovement() {
+// 	var paddle2YCenter = paddle2Y + (PADDLE_HEIGHT/2);
+// 	if(paddle2YCenter < ballY - 35) {
+// 		paddle2Y = paddle2Y + 6;
+// 	} else if(paddle2YCenter > ballY + 35) {
+// 		paddle2Y = paddle2Y - 6;
+// 	}
+// }
 
 // function keyReleased() {
 //     left.move(0);
